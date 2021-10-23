da70d14b2940e4b754e40f1e54a7c22056e064c5
Jakub Hubisz <jakub.hubisz@gmail.com>
2021-10-23 08:41:01 +0200

    Adding test project and initial test

---
+++ SnakesAndLaddersTests/BoardTests.cs
@@ -0,0 +1,14 @@
+using SnakesAndLadders;
+using Xunit;
+
+namespace SnakesAndLaddersTests
+{
+    public class BoardTests
+    {
+        [Fact]
+        public void BoardIsCreated()
+        {
+            var board = new Board();
+        }
+    }
+}
