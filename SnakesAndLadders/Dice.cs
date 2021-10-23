0bd7e0f111a14e2103d1bb4cb97c2a01d81c2aff
Jakub Hubisz <jakub.hubisz@gmail.com>
2021-10-23 21:55:06 +0200

    Adding Dice class

---
+++ SnakesAndLadders/Dice.cs
@@ -0,0 +1,10 @@
+﻿namespace SnakesAndLadders
+{
+    public class Dice : IDice
+    {
+        public int RollTheDice()
+        {
+            return 1;
+        }
+    }
+}
---
+++ SnakesAndLaddersTests/DiceTests.cs
@@ -0,0 +1,16 @@
+﻿using SnakesAndLadders;
+using Xunit;
+
+namespace SnakesAndLaddersTests
+{
+    public class DiceTests
+    {
+        [Fact]
+        public void DiceIsCreatedAndOfTypeIDice()
+        {
+            var dice = new Dice();
+
+            Assert.IsAssignableFrom<IDice>(dice);
+        }
+    }
+}
